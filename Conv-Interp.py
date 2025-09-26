6a714e817b786878a80bc61f7e9080ad3cf066d0
Acabradabra <francoismuller5@gmail.com>
2025-09-26 15:02:23 +0200

    Interp

--- Conv-Interp.py
+++ Conv-Interp.py
@@ -16,28 +16,60 @@ t0=time.time()
 #                     Parameters
 #===================================================================================
 
-file_in='/mnt/scratch/PRECIZE/Sandia-Jaravel/RUN-D100-02-Laera/INIT-Fluent/Interp-00-Fluent.ip'
+# file_in='/mnt/scratch/PRECIZE/Sandia-Jaravel/RUN-D100-02-Laera/INIT-Fluent/Interp-00-Fluent.ip'
+# file_in='/mnt/scratch/PRECIZE/Sandia-Jaravel/RUN-D100-02-Laera/DUMP-02-EDC-PB-OD2-FD3/DATA/Interp-EDC-FD3.ip'
 file_ou=file_in[:-3]+'-New.ip'
 
 #===================================================================================
-util.Section('=> Reading : {:.3f}'.format(time.time()-t0),2,5,'r')
+util.Section('Reading : {:.3f}'.format(time.time()-t0),1,5,'r')
 #===================================================================================
-
 fin=open(file_in,'r')
 Lfin=fin.readlines()
 fin.close()
+util.Section('Reading 1 : {:.3f}'.format(time.time()-t0),0,1,'b')
 
-Nd=int(float(Lfin[1]))
-Np=int(float(Lfin[2]))
-Nv=int(float(Lfin[3]))
+Nd=int(Lfin[1])
+Np=int(Lfin[2])
+Nv=int(Lfin[3])
 Nl=len(Lfin)
 
-Fields=[ f[:-1] for f in Lfin[4:4+Nv]]
+Fields_in=[ f[:-1] for f in Lfin[4:4+Nv]]
 
 print( '=> Nd={:d}, Np={:d}, Nv={:d}, Nl={:d}'.format(Nd,Np,Nv,Nl) )
-print( '=> Fields : {}'.format(Fields) )
+print( '=> Fields_in : {}'.format(Fields_in) )
 
-DATA=zeros((Np,Nv+Nd))
+DATA_in=zeros((Np,Nv+Nd))
 for n in range(Nv+Nd) :
-    Sv=
-    DATA[:,n]=array( [ float(Lfin[4+Nv+i].split()[n]) for i in range(Np) ] )
+    n0=4+Nv+n*(Np+1)
+    Sv=Lfin[n0:n0+Np] ; Sv[0]=Sv[0][1:]
+    DATA_in[:,n]=array( [float(s) for s in Sv ] )
+
+#===================================================================================
+util.Section('Processing : {:.3f}'.format(time.time()-t0),1,5,'r')
+#===================================================================================
+
+DATA_ou=DATA_in.copy()
+
+#===================================================================================
+util.Section('Writing : {:.3f}'.format(time.time()-t0),1,5,'r')
+#===================================================================================
+
+Fields_ou=Fields_in.copy()+['fmean']
+Nv=len(Fields_ou)
+
+fou=open(file_ou,'w')
+fou.write( '3\n' )
+fou.write( '{}\n'.format(Nd) )
+fou.write( '{}\n'.format(Np) )
+fou.write( '{}\n'.format(Nv) )
+for f in Fields_ou :
+    fou.write( '{}\n'.format(f) )
+for v in range(Nv) :
+    fou.write('(')
+    for p in range(Np) : fou.write( ' {:.12e}\n'.format(DATA_ou[p,v]) )
+    fou.write(')\n')
+fou.close()
+
+#===================================================================================
+util.Section('Programme completed : {:.3f}'.format(time.time()-t0),1,5,'r')
+#===================================================================================
